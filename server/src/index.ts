496bad1aa6101a2412bbd90fb15c310569b7c5fe
gafredo <gafredo@gmail.com>
2024-10-24 20:21:35 -0300

    server modifications

--- server/src/index.ts
+++ server/src/index.ts
@@ -31,7 +31,7 @@ app.use('/search', searchRoutes);
 app.use('/users', usersRoutes);
 app.use('/teams', teamRoutes);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3000;
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port} 🚀🚀🚀🚀`);
 });
